a97ef7a3a4d15d1e872794cbb1a87e7aa732b20f
Madone <ahamad.jedu@gmail.com>
2016-09-09 09:12:33 +0700

    New Module

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace suPnPsu\borrowMaterial;
+
+/**
+ * borrowMaterial module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'suPnPsu\borrowMaterial\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
